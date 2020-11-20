82e4d402e513a678d73689c3b1622633bcf256a4
miftah <miftah.it2@gmail.com>
2020-11-20 15:31:52 +0700

    finish footer & side info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,15 +7,27 @@ import Footer from './components/Footer';
 import ModalMenu from './components/ModalMenu';
 
 function App() {
-
   const [isOpen, setValue] = useState(false);
   return (
     <div className="App">
       <Router>
-        <Header isOpen={isOpen}/>
-        <ModalMenu setValue={setValue} isOpen={isOpen}/>
-        <main className="main"
->asdasdasdasd</main>        <Footer/>
+        <Header isOpen={isOpen} />
+        <div class="sideinfo">
+          <div class="sideinfo__inner">
+            <p class="sideinfo__summary">
+              <span class="sideinfo__summary-text">
+                <span class=" type-outer" id="type-summary">
+                  <span class="Typewriter__wrapper">Visual Creator Coll</span>
+                </span>
+              </span>
+            </p>
+          </div>
+        </div>
+        <ModalMenu setValue={setValue} isOpen={isOpen} />
+        <main className="main" style={{ height: '1000px' }}>
+          asdasdasdasd
+        </main>
+        <Footer />
       </Router>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,85 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import Logo from '../paper-plane.svg'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../paper-plane.svg';
+import LogoWhite from '../paper-plane-white.svg';
 
 const Footer = () => {
-  return(
+  return (
     <footer className="footer">
       <div className="footer__inner">
-        <div className="footer__sitemenu">asd</div>
+        <div className="footer__sitemenu sitemenu">
+          <div className="sitemenu__content">
+            <div className="sitemenu__block">
+              <div className="sitemenu__index">
+                <div className="sitemenu__index-text">
+                  <Link to="/">Top</Link>
+                </div>
+              </div>
+            </div>
+            <div className="sitemenu__block">
+              <div className="sitemenu__index">
+                <div className="sitemenu__index-text">
+                  <Link to="/">Service</Link>
+                </div>
+              </div>
+              <div className="sitemenu__detail">
+                <div className="sitemenu__detail-list">
+                  <Link to="/">Design</Link>
+                </div>
+                <div className="sitemenu__detail-list">
+                  <Link to="/">Development</Link>
+                </div>
+                <div className="sitemenu__detail-list">
+                  <Link to="/">Direction</Link>
+                </div>
+              </div>
+            </div>
+            <div className="sitemenu__block">
+              <div className="sitemenu__index">
+                <div className="sitemenu__index-text">
+                  <Link to="/">About</Link>
+                </div>
+              </div>
+            </div>
+            <div className="sitemenu__block">
+              <div className="sitemenu__index">
+                <div className="sitemenu__index-text">
+                  <Link to="/">Case Study</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="footer__logo">
           <div className="footer__logo-icon">
             <NavLink to="/">
-            <img src={Logo}/>
+              <img src={LogoWhite} />
             </NavLink>
           </div>
           <div className="footer__logo-desc">
             <Link to="/">
-            2-14-27 2F Hiroo, Shibuya-ku, Tokyo 
-            <br/>
-            150-0012
+              2-14-27 2F Hiroo, Shibuya-ku, Tokyo
+              <br />
+              150-0012
             </Link>
           </div>
           <div className="footer__sns">
-            asdsad
+            <Link to="/">
+              <i className="la la-twitter" />
+            </Link>
+            <Link to="/">
+              <i className="la la-instagram" />
+            </Link>
           </div>
           <div className="footer__copyright">
-<div className="footer__copyright-text">
-© 2020 CCC management inc.
-</div>
+            <div className="footer__copyright-text">© 2020 CCC management inc.</div>
           </div>
-
         </div>
       </div>
       <div className="footer__contact">
         <div className="footer__contact-btn">
           <Link className="footer__contact-link" to="/">
-            <span className="footer__contact-text">
-              Contact Us
-            </span>
+            <span className="footer__contact-text">Contact Us</span>
           </Link>
         </div>
         <div className="footer__contact-panel">
@@ -48,7 +91,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
